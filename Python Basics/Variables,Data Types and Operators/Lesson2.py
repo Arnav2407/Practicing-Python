f77f1f8a8167439d057352e1f763a81575f877cd
Arnav2407 <bhatnagararnav61@gmail.com>
2026-06-04 19:01:47 +0530

    Fourth

---
+++ Python Basics/Variables,Data Types and Operators/Lesson2.py
@@ -0,0 +1,22 @@
+#problem1
+length = 15
+width = 8
+AreaOfRectangle = length*width
+print(AreaOfRectangle)
+
+#problem2
+score1 = 85.5
+score2 = 92.0
+score3 = 78.5
+avg = (score1+score2+score3)/3
+print(avg)
+
+#problem3
+xyz = "Python is fun "
+pqr = "Let's learn it"
+my_string = xyz+pqr
+print(my_string)
+
+#problem4
+num = 5
+print(num > 10 and num < 20)
