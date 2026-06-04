4c7a6013a227cfb75e0f3827abf0b0836d423b03
Arnav2407 <bhatnagararnav61@gmail.com>
2026-06-04 18:55:15 +0530

    third

--- Python Basics/Variables,Data Types and Operators/Lesson1.py
+++ Python Basics/Variables,Data Types and Operators/Lesson1.py
@@ -1,1 +1,12 @@
 quantity = 100
+language = "Python"
+is_active = False
+x,y,z = 5,5,5
+a,b,c = 10,"hello",3.14
+counter = 0
+counter += 1
+print(counter)
+counter += 1
+print(counter)
+counter += 1
+print(counter)
